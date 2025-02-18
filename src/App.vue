e2474df0fbc4a62e357be08d6cc85b36dd98b382
Iker Fernández <148960996+IkerFerCol@users.noreply.github.com>
2025-02-18 12:53:03 +0100

    18/2/2025

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,39 @@
 <script>
-import axios from "axios";
+import axios from "axios"
 export default {
-  data(){
+  data() {
     return {
-      nom_web: "Lista de ciudades",
-      ciudades: [
-        {
-          "id": 1,
-          "nom": "La Vall d'uixó",
-          "descripcion": "Ciudad la Vall",
-          "imagen": "imagen1.jpg",
-          "poblacion": 20000,
-          "franquicias": [],
-          "provincia_id": {
-            "id": 1,
-            "nom": "Castelló",
-            "pais_id": {
-              "id": 1,
-              "nom": "Espanya"
-            }
-          }
-        },
-        {
-          "id": 2,
-          "nom": "Fondeguilla",
-          "descripcion": "Ciudad Fondeguilla",
-          "imagen": "imagen2.jpg",
-          "poblacion": 5000,
-          "franquicias": [],
-          "provincia_id": {
-            "id": 1,
-            "nom": "Castelló",
-            "pais_id": {
-              "id": 1,
-              "nom": "Espanya"
-            }
-          }
-        }
-        ]
+      nombre_web: "Lista de ciudades",
+      ciudades: []
     }
   },
   mounted() {
-    axios.get("http://localhost:8082/api/ciudades").then(response => {
-      let ciudades = response.data.results;
-       let result = [];
-      for (let ciudad of ciudades){
-        axios.get(ciudad.url).then(response_details => {
-          let ciud = response_details.data;
-          result.push(ciud);
-        })
-        console.log(ciudad);
-      }
-
-
+    axios.get("http://localhost:8082/ciudads/ciudades").then(res => {
+      this.ciudades = res.data;
     })
   }
 }
+
 </script>
 
 <template>
-  <h1>{{ nom_web }}</h1>
+  <h1>{{nombre_web}}</h1>
 
   <table>
     <thead>
     <tr>
+      <th>Id</th>
       <th>Nombre</th>
-      <th>Poblacion</th>
-      <th>Descripcion</th>
+      <th>Población</th>
+      <th>Descripción</th>
       <th>Imagen</th>
     </tr>
     </thead>
     <tbody>
     <tr v-for="ciudad in ciudades">
-      <td>{{ ciudad.nom }}</td>
-      <td>{{ ciudad.poblacion }}</td>
+      <td>{{ ciudad.id }}</td>
+      <td>{{ ciudad.nombre }}</td>
+      <td>{{ ciudad.poblacion}}</td>
       <td>{{ ciudad.descripcion }}</td>
       <td>{{ ciudad.imagen }}</td>
     </tr>
